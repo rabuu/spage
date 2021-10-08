44e57c2803efbfc9b612521ec967f44d5ce4dbed
rabuu <rabuu.dev@gmail.com>
2021-10-09 00:16:52 +0200

    Polychrome/monochrome

--- main.js
+++ main.js
@@ -29,21 +29,59 @@ function themeBtnPressed(x) {
 
 // LINKS SWITCH
 function linksSwitchChanged(checkbox) {
+	var links = document.getElementsByClassName("links");
 	if (checkbox.checked) {
-		document.getElementById("links").style.display = "flex";
+		for (var i = 0; i < links.length; i++) {
+			links[i].style.display = "flex";
+		}
 		localStorage.setItem("showLinks", "flex");
 	} else {
-		document.getElementById("links").style.display = "none";
+		for (var i = 0; i < links.length; i++) {
+			links[i].style.display = "none";
+		}
 		localStorage.setItem("showLinks", "none");
 	}
 }
 
 function applyLinksSwitch() {
 	var showLinks = localStorage.getItem("showLinks");
-	document.getElementById("links").style.display = showLinks;
+	var links = document.getElementsByClassName("links");
+	for (var i = 0; i < links.length; i++) {
+		links[i].style.display = showLinks;
+	}
 	document.getElementById("links-checkbox").checked = (showLinks != "none");
+}
+
+// LINKS COLOR SWITCH
+function setLinksColor(shouldMonochrome) {
+	var monochromes = document.getElementsByClassName("monochrome");
+	var polychromes = document.getElementsByClassName("polychrome");
+	if (shouldMonochrome) {
+		for (var i = 0; i < monochromes.length; i++) {
+			monochromes[i].style.visibility = "visible";
+		}
+		for (var i = 0; i < polychromes.length; i++) {
+			polychromes[i].style.visibility = "hidden";
+		}
+		localStorage.setItem("linksColor", "monochrome");
+	} else {
+		for (var i = 0; i < monochromes.length; i++) {
+			monochromes[i].style.visibility = "hidden";
+		}
+		for (var i = 0; i < polychromes.length; i++) {
+			polychromes[i].style.visibility = "visible";
+		}
+		localStorage.setItem("linksColor", "polychrome");
+	}
+}
+
+function applyLinksColorSwitch() {
+	var linksColor = localStorage.getItem("linksColor");
+	setLinksColor(linksColor == "monochrome");
+	document.getElementById("links-color-checkbox").checked = (linksColor == "monochrome");
 }
 
 // ON STARTUP
 setTheme(localStorage.getItem("theme"));
 applyLinksSwitch();
+applyLinksColorSwitch();
